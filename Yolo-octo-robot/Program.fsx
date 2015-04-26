6a0409dfc273767a35e36a3489aad8c00468e121
Jollr <jolle.lont@gmail.com>
2015-04-26 23:34:38 +0200

    webserver

--- Yolo-octo-robot/Program.fsx
+++ Yolo-octo-robot/Program.fsx
@@ -7,6 +7,13 @@ open Suave.Types
 
 open System.Net
 
+let pokePage pokeName = "<html><head></head><body><form method=\"POST\"><input type=\"submit\" value=\"" + pokeName + "\"/></form</body></html>" 
+
+let pokeButton pokeName = 
+    do sprintf pokeName
+    let result = OK "Koning Bauke Jan is de mol"
+    result
+
 let config =
   { defaultConfig with
      bindings = [ HttpBinding.mk' HTTP "192.168.1.12" 80 ] }
@@ -15,6 +22,9 @@ let app =
   choose
     [ GET >>= choose
         [ path "/kevin" >>= OK "Hallo Kevin!"
-          path "/ryanne" >>= OK "Hallo Ryanne!" ] ]
+          path "/ryanne" >>= OK "Hallo Ryanne!" 
+          path "/bulbasaur" >>= OK (pokePage "bulbasaur") ] 
+      POST >>= choose 
+        [ path "/bulbasaur" >>= OK "You pressed Bulbasaur!"] ]
 
 startWebServer config app
