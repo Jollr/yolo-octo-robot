cf3b8912319a3f4f8323d68d9ebd563b0fdb7cb6
Jolle Lont <jolle.lont@malengo.nl>
2015-05-06 10:12:37 +0200

    force pokebutton re-evaluation on POST

--- PokePages.fs
+++ PokePages.fs
@@ -4,15 +4,15 @@ open Suave.Http
 open Suave.Http.Applicatives
 open Suave.Http.Successful
 open Suave.Http.Files
+open Suave.Types
 
 open Puzzle
 
-let buttonElement pokeName = 
-    "<button type='submit'><img src='" + pokeName + ".png'/></button>"
-
+let buttonElement pokeName = "<button type='submit'><img src='" + pokeName + ".png'/></button>"
 let pokePage pokeName = "<html><head></head><body><form method='POST'>" + (buttonElement pokeName) + "</form></body></html>" 
 
 let pokeButton pokeName = 
+    printfn "%s" pokeName
     Puzzle.trigger 1 1
     OK pokeName
 
@@ -21,4 +21,4 @@ let pokeBindings =
         [ path "/bulbasaur" >>= OK (pokePage "bulbasaur")
           path "/bulbasaur.png" >>= file "img/bulbasaur.png" ] 
       POST >>= choose 
-        [ path "/bulbasaur" >>= pokeButton "Bulbasaur"] ]
+        [ path "/bulbasaur" >>= context (fun x -> pokeButton "Bulbasaur") ] ]
