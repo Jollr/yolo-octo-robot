96c760491dc7b9fe2ede1a54f01d467337dae837
Jolle Lont <jolle.lont@malengo.nl>
2015-05-06 14:12:32 +0200

    cleanup

--- PokePages.fs
+++ PokePages.fs
@@ -14,23 +14,15 @@ open PokeMappings
 
 let private buttonElement (pokeName:string) = "<button type='submit'><img src='" + pokeName + "/img'/></button>"
 let private buttonPage (pokemon: PokeRegistration) = "<html><head></head><body><form method='POST'>" + (buttonElement pokemon.Name) + "</form></body></html>" 
-let private pokeImage (pokeName: string) = "img/" + pokeName + ".png"
+let private pokeImage (pokeName: string) = file ("img/" + pokeName + ".png")
 
 let private getPokePage (id: Guid) =
     match PokeMappings.GetById id with
     | Some pokemon -> OK (buttonPage pokemon)
     | None -> NOT_FOUND ""
 
-//let private trigger pokeName = 
-//    printfn "%s" pokeName
-//    Puzzle.trigger 1 1
-//    OK pokeName
-
 let private postPokePage (id: Guid) =
-    let str = id.ToString()
-    printfn "%s" str
     let trigger (pokemon: PokeRegistration) = 
-        printfn "%s" pokemon.Name
         Puzzle.trigger pokemon.X pokemon.Y
         OK pokemon.Name
      
@@ -41,7 +33,7 @@ let private postPokePage (id: Guid) =
 
 let pokeBindings = 
     [ GET >>= choose
-        [ pathScan "/pokemon/%s/img" ( fun(pokeName) -> file (pokeImage pokeName) )
+        [ pathScan "/pokemon/%s/img" pokeImage
           guidScan "/pokemon/" getPokePage ] 
       POST >>= choose 
         [ guidScan "/pokemon/" postPokePage] ]
